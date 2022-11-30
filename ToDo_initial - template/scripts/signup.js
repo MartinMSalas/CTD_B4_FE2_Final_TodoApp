04cdd3ebd797e68cd93126e0129f5ba9ffa2b812
MartinMSalas <ingmartinmsalas@gmail.com>
2022-11-29 21:03:59 -0300

    adding starting validation

--- ToDo_initial - template/scripts/signup.js
+++ ToDo_initial - template/scripts/signup.js
@@ -1,13 +1,141 @@
 window.addEventListener("load", function () {
   /* ---------------------- obtenemos variables globales ---------------------- */
+  const form = document.forms[0];
+  const nombre = document.querySelector("#inputNombre");
+  const apellido = document.querySelector("#inputApellido");
+  const email = document.querySelector("#inputEmail");
+  const password = document.querySelector("#inputPassword");
+  const passwordRepetida = document.querySelector("#inputPasswordRepetida");
 
+  const url = "http://todo-api.ctd.academy:3000/v1";
   /* -------------------------------------------------------------------------- */
   /*            FUNCIÓN 1: Escuchamos el submit y preparamos el envío           */
   /* -------------------------------------------------------------------------- */
-  form.addEventListener("submit", function (event) {});
+  
+  form.addEventListener("submit", function (event) {
+    // prevenimos el envio por defecto del formulario
+    event.preventDefault();
 
+    // creamos el cuerpo de la request
+
+    const payload = {
+        "firstName": nombre.value,
+        "lastName": apellido.value,
+        "email": email.value,
+        "password": password.value
+    };
+    // Validacion formulario
+
+    function validarNombre(){
+      return (isNaN(nombre.value) && nombre.value.length >3)
+      
+    }
+    function validarEmail() {
+      let resultado = false;
+    
+      // // EJEMPLO VALIDACIÓN A MANO 👇
+      // if (email.includes('@') && email.includes('.') && !email.includes(' ') && email.length > 5) {
+      //     resultado = true;
+      // }
+    
+      // EJEMPLO CON EXPRESION REGULAR 👇
+      let regex = new RegExp("[a-z0-9]+@[a-z]+.[a-z]{2,3}");
+    
+      if (regex.test(email)) {
+        resultado = true;
+      }
+    
+      return resultado;
+    }
+    
+    function validarPassword() {
+      let resultado = false;
+    
+      // si pasa las pruebas lo damos por válido 👇
+      if (password.length > 5 && !password.includes(" ") && password.value === passwordRepetida.value) {
+        resultado = true;
+      }
+      
+      return resultado;
+    }
+    
+
+    //aca lo valido!!
+    function validacion(){
+      console.log("soy la validacion")
+      console.log(validarEmail());
+      console.log(validarNombre());
+      console.log(validarPassword());
+      if (validarNombre() && validarEmail() && validarPassword()){
+        return true;
+      }
+
+      return false
+      
+    }
+     
+    
+    // con el formulario validado
+  
+
+    // creamos las configuraciones de la request para el fetch
+    const settings = {
+      method: "POST",
+      body: JSON.stringify(payload),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+
+    // lanzamos la request a la API
+    if(validacion()){
+      console.log("la validacion fue verdadera");
+      console.log(password.value);
+      console.log(passwordRepetida.value);
+      realizarRegister(settings);
+    }
+    
+    
+  });
+  
+  
+  
   /* -------------------------------------------------------------------------- */
   /*                    FUNCIÓN 2: Realizar el signup [POST]                    */
   /* -------------------------------------------------------------------------- */
-  function realizarRegister(settings) {}
+  function realizarRegister(settings) {
+    
+
+    console.log("Lanzamos la registracion a la API...");
+    // hacemos el fetch, le pasamos los dos parametros que necesita
+    
+    /* de aca hacia modificar */
+    
+    fetch(`${url}/users/login`, settings)
+      .then((response) => {
+        // captamos la respuesta "cruda" de la API (promesa)
+        console.log(response);
+        // chequeamos si salio todo mal
+        if (response.ok != true) {
+          alert("Algo malió sal.");
+        }
+        return response.json(); // transformamos de JSON a objeto JS
+      })
+
+      /*
+      data = {
+        jwt : "ajsadjadsjdasjksd"
+      }
+      */
+      .then((data) => {
+        // obtenemos el resultado del .then anterior, la rta "procesada", podemos manipular los datos
+        console.log("Promesa cumplida");
+        console.log(data);
+        if (data.jwt) {
+          // guardamos el token en el local storage
+          localStorage.setItem("jwt", JSON.stringify(data.jwt));
+          location.replace("./mis-tareas.html");
+        }
+      });
+  }
 });
